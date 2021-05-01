f810538a8d44815654585d3dc0f1dbf3c447bf76
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-05-01 16:40:10 +0530

    Update Lead Game.cpp

--- Lead Game.cpp
+++ Lead Game.cpp
@@ -1,32 +1,16 @@
-#include "bits/stdc++.h"
-
-#define gOdSpEeD ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(0)
-#define Int long long
-#define all(x) x.begin(), x.end()
-#define el '\n'
-#define ints unsigned
-#define cy cout << "YES" << end 
-#define cn cout <<"NO" << end
-#define elif else if
-#define test int t; cin>>t; while(t--)
-
 using namespace std;
-
-int main () {
-    gOdSpEeD;
-    int n ;cin>>n;
-    int lead{};
-    int player{};
-    int xlead{};
-    int ylead{};
-    for(int i {};i<n;i++){
-    	int p1,p2;
+#include "bits/stdc++.h"
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);cout.tie(nullptr);
+	long long n ;cin>>n;
+    long long l=0,x=0,p=0,y{};
+    for(long long p1,p2,i {};i<n;i++){
     	cin>>p1>>p2;
-    	xlead += p1;
-    	ylead += p2;
-    	if(abs(xlead-ylead)>lead){lead = abs(xlead-ylead);(xlead>ylead?player=1:player=2);}
-    }//test
-    cout << player << " " << lead << endl;
-
-    return 0;
+    	x += p1;
+    	y += p2;
+    	if(abs(x-y)>l){l = abs(x-y);(x<y? p=2:p=1);}
+    }
+    cout << p << " " << l << endl;
+	return 0;
 }
